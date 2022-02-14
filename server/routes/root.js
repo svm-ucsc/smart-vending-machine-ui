557630f32ca922d33a12e364d5026097a8de5f7d
Lena <lena.hicksonlong@gmail.com>
2022-02-13 22:57:22 -0800

    change start to all addresses

--- server/routes/root.js
+++ server/routes/root.js
@@ -2,7 +2,7 @@
 
 module.exports = async function (fastify, opts) {
   fastify.get('/', async function (request, reply) {
-    reply.sendFile('index.html')
+    return reply.sendFile('index.html')
   })
 
 }
