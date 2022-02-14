615c552e3406160e1ec66e888f7cbae83c82b8cc
Lena <lena.hicksonlong@gmail.com>
2022-02-14 01:47:29 -0800

    reset test

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,5 +1,5 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png" style="transform:rotate(180deg);">
+  <img alt="Vue logo" src="./assets/logo.png">
   <HelloWorld msg="Welcome to Your Vue.js App"/>
 </template>
 
