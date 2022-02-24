f991fffe4bb0a41ec909d39840024fb45eae58c4
cngcruz <christopher.ng.cruz@gmail.com>
2022-02-24 11:15:36 -0800

    fixing horizontal scrolling

--- src/App.vue
+++ src/App.vue
@@ -3,8 +3,9 @@
     <div class="row p-0">
       <search-bar />
     </div>
-    <div class="container scroll">
-      <div class="row d-flex menu_box">
+
+    <div>
+      <div class="row menu_box">
         <div class="col-6 item_box">
           <item-detail :item-id="1" />
         </div>
@@ -16,12 +17,32 @@
         </div>
       </div>
     </div>
-    <div class="row d-flex">
-      <div class="col-6 item_box">
-        <item-detail :item-id="1" />
+
+    <div>
+      <div class="row menu_box">
+        <div class="col-6 item_box">
+          <item-detail :item-id="1" />
+        </div>
+        <div class="col-6 item_box">
+          <item-detail :item-id="2" />
+        </div>
+        <div class="col-6 item_box">
+          <item-detail :item-id="3" />
+        </div>
       </div>
-      <div class="col-6 item_box">
-        <item-detail :item-id="2" />
+    </div>
+
+    <div>
+      <div class="row menu_box">
+        <div class="col-6 item_box">
+          <item-detail :item-id="1" />
+        </div>
+        <div class="col-6 item_box">
+          <item-detail :item-id="2" />
+        </div>
+        <div class="col-6 item_box">
+          <item-detail :item-id="3" />
+        </div>
       </div>
     </div>
   </div>
@@ -35,6 +56,18 @@
     background: $summer-yellow;
     height: 100%;
     width: 100%;
+
+    .menu_box{
+      overflow-x: scroll;
+      flex-wrap: nowrap;
+    }
+
+    .item_box{
+      padding: 0 4%; 
+      margin-right: -1%;
+      display: inline-block;
+      float: none;
+    }    
   }
   .app_title{
     font-size: 45px;
@@ -43,20 +76,5 @@
   .row{
     padding: 20px 0;
   }
-
-  .item_box{
-    padding: 0 4%; 
-    margin-right: -1%;
-    display: inline-block;
-    float: none;
-  }
-
-  .menu_box{
-    overflow-x: scroll;
-    overflow-y: hidden;
-    white-space: nowrap;
-    
-  }
-
 
 </style>
