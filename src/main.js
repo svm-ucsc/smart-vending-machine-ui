2fbba1eb16e4762d5658fe8c7162c79f55058261
Hanna Alanizi <halanizi@ucsc.edu>
2022-03-03 15:55:59 -0800

    merging local changes

--- src/main.js
+++ src/main.js
@@ -22,19 +22,32 @@ const store = createStore({
     mutations: {
         insertToCart(state, data){
             // data will be an object pushed onto cartInfo array that holds {foodName, quantity}
+            // if(state.cartInfo.length > 0){ // if list isn't empty, we need to look through it first
+            //     let temp = state.cartInfo.find(o => o.foodName === data.foodName); // temp returns an object so we need to parse it's properties
+            //     // if the element exists in the cart already and it matches what we want to update it, just update the quantity instead
+            //     if((temp != null) && (temp.foodName === data.foodName)){
+            //         temp.quantity = data.quantity;
+            //     }else{ // element doesn't exist, so push it
+            //         state.cartInfo.push(data);
+            //     }
+               
+            // }else{ // empty list condition
+            //     state.cartInfo.push(data);
+            // }
             if(state.cartInfo.length > 0){ // if list isn't empty, we need to look through it first
-                let temp = state.cartInfo.find(o => o.foodName === data.foodName); // temp returns an object so we need to parse it's properties
-
+                let temp = state.cartInfo.find(o => o.itemId === data.itemId); // temp returns an object so we need to parse it's properties
                 // if the element exists in the cart already and it matches what we want to update it, just update the quantity instead
-                if((temp != null) && (temp.foodName === data.foodName)){
-                    temp.quantity = data.quantity;
+                if((temp != null) && (temp.itemId === data.itemId)){
+                    temp.quantity += data.quantity;
                 }else{ // element doesn't exist, so push it
                     state.cartInfo.push(data);
                 }
+                // temp.quantity = 0;
                
             }else{ // empty list condition
                 state.cartInfo.push(data);
             }
+           
         },
         removeFromCart(state, indexToDelete){
             // data stores the index of where the element is to be removed
