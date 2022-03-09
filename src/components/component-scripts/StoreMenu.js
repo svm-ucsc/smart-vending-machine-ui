ec3a5af87aad4d27b20f552534a3ed33e43ab829
Hanna Alanizi <hannaalanizi@gmail.com>
2022-03-08 18:39:40 -0800

    updated to .join() per Chris' request :)

--- src/components/component-scripts/StoreMenu.js
+++ src/components/component-scripts/StoreMenu.js
@@ -4,7 +4,7 @@ export default {
     async fetchingNameId(){
       try {
         const response = await axios.get('http://ec2-54-167-36-58.compute-1.amazonaws.com:3000/item', 
-        { params: { fields:["item_id", "name"] } });
+        { params: { fields:["item_id", "name"].join() } });
         const obj = response.data;
 
         let parsedData = obj;
