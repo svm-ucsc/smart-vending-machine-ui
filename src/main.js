faa9c3699ef7e71a104e3992c582cba05786a16a
Hanna Alanizi <hannaalanizi@gmail.com>
2022-03-10 21:02:24 -0800

    added font awesome icons for shopping cart and other icon features :)

--- src/main.js
+++ src/main.js
@@ -8,6 +8,12 @@ import SearchBar from './components/SearchBar.vue';
 import CartItems from './components/CartItems.vue';
 import StoreMenu from './components/StoreMenu.vue';
 
+// font awesome icons added
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { fas } from '@fortawesome/free-solid-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+library.add(fas)
+
 const axios = require('axios');
 
 // Holds global data for application
@@ -66,5 +72,6 @@ app.component('ItemDetail', ItemDetail);
 app.component('SearchBar', SearchBar);
 app.component('CartItems', CartItems);
 app.component('StoreMenu', StoreMenu);
+app.component('font-awesome-icon', FontAwesomeIcon);
 
 app.mount('#app')
