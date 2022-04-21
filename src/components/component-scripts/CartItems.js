0df60d00f68ae40f83e8aa574a1506cc4d068a1b
Hanna Alanizi <hannaalanizi@gmail.com>
2022-04-20 18:11:38 -0700

    created new branch for merging main with loading screen

--- src/components/component-scripts/CartItems.js
+++ src/components/component-scripts/CartItems.js
@@ -1,23 +1,23 @@
 import $ from 'jquery'
 
-export default{
+export default {
     data() {
-        return{
+        return {
             toggle: false,
             modal_paymentScreen: false,
             modal_pageCount: 0,
             cartReceipt: []
         }
-    },  
+    },
     methods: {
-        showCart(){
+        showCart() {
             const btn = document.getElementById('reviewOrderBtn');
             $(btn).css('visibility', 'hidden');
             this.toggle = true;
             const collapseBtn = document.getElementById('collapseCartBtn');
             $(collapseBtn).css('visibility', 'visible');
         },
-        collapseCart(){
+        collapseCart() {
             const btn = document.getElementById('reviewOrderBtn');
             $(btn).css('visibility', 'visible');
             this.toggle = false;
@@ -25,56 +25,56 @@ export default{
             $(collapseBtn).css('visibility', 'hidden');
 
         },
-        placeOrder(){
+        placeOrder() {
             // copy order to a receipt list prior to the reset of content
-            for(let i of this.$store.state.cartInfo){
+            for (let i of this.$store.state.cartInfo) {
                 this.cartReceipt.push(i);
             }
-            this.$store.commit('sendOrderToDB');            
+            this.$store.commit('sendOrderToDB');
         },
-        onSuccess(){
+        onSuccess() {
             this.modal_pageCount++;
 
         },
-        nextSplitModal(){
+        nextSplitModal() {
             this.modal_pageCount++;
             this.modal_paymentScreen = true;
-                 
+
         },
-        prevSplitModal(){
+        prevSplitModal() {
             this.modal_pageCount--;
             this.modal_paymentScreen = false;
         },
-        backToHome(){
+        backToHome() {
             this.modal_pageCount = 0;
         },
-        costMultiplier(itemPrice, quantity){
-            let dollars = (itemPrice * quantity)/100;
-            dollars = dollars.toLocaleString("en-US", {style:"currency", currency:"USD"});
+        costMultiplier(itemPrice, quantity) {
+            let dollars = (itemPrice * quantity) / 100;
+            dollars = dollars.toLocaleString("en-US", { style: "currency", currency: "USD" });
             return dollars;
         },
-        getTotalAmount(cartInfo){
+        getTotalAmount(cartInfo) {
             // pass by reference taking local shopping basket and placing in Vuex, then Vuex will globally store the subtotal and then this function will grab a copy of it
             this.$store.commit('calculateTotalCost', cartInfo);
-            let local_subTotal = (this.$store.subTotal)/100;
-            local_subTotal = local_subTotal.toLocaleString("en-US", {style:"currency", currency:"USD"});
+            let local_subTotal = (this.$store.subTotal) / 100;
+            local_subTotal = local_subTotal.toLocaleString("en-US", { style: "currency", currency: "USD" });
             return local_subTotal;
         }
     },
-    mounted: function(){
+    mounted: function () {
         // need arrow operator because the scope of "this" changes otherwise
         // any time the modal is clicked out of, we reset the modal page counter
-        document.getElementById('cartModalReviewScreen').addEventListener("hidden.bs.modal",() => {
-            this.modal_pageCount = 0;          
+        document.getElementById('cartModalReviewScreen').addEventListener("hidden.bs.modal", () => {
+            this.modal_pageCount = 0;
         });
 
-        let nextBtn = document.querySelector('.CartItems__nextBtn');        
-        if (this.modal_pageCount === 1){
-            if(nextBtn){
-                nextBtn.style.visibility="hidden";
+        let nextBtn = document.querySelector('.CartItems__nextBtn');
+        if (this.modal_pageCount === 1) {
+            if (nextBtn) {
+                nextBtn.style.visibility = "hidden";
             }
-        }else{
-            nextBtn.style.visibility="visible";
+        } else {
+            nextBtn.style.visibility = "visible";
         }
     }
 }
